33b433c6ef60a3f089223b229a3258aa315d87dc
gnobre <gustavognobre@gmail.com>
2025-02-15 19:28:56 -0300

    gender undefined

--- src/components/auth/RegisterForm.component.tsx
+++ src/components/auth/RegisterForm.component.tsx
@@ -35,7 +35,7 @@ export const RegisterForm = () => {
             confirmPassword: "",
             name: "",
             dateBirth: "",
-            gender: "",
+            gender: undefined,
             photo: "",
         },
     });
