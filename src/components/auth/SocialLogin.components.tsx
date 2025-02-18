c3ab34cec3ee54101f45d165e56aa0be67340798
gnobre <gustavognobre@gmail.com>
2025-02-18 18:59:06 -0300

    oauth github and google

--- src/components/auth/SocialLogin.components.tsx
+++ src/components/auth/SocialLogin.components.tsx
@@ -1,18 +1,37 @@
 "use client";
 
 import { Button } from "@//components/ui/button";
-import { FaFacebook } from "react-icons/fa";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { signIn } from "next-auth/react";
+import { FaGithub } from "react-icons/fa6";
+
 import { FcGoogle } from "react-icons/fc";
 
 export const Social = () => {
-  return (
-    <div className=" flex items-center w-full gap-x-2">
-      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
-        <FcGoogle className="w-5 h-5" />
-      </Button>
-      <Button size="lg" className="w-full" variant="outline" onClick={() => {}}>
-        <FaFacebook className="w-5 h-5" />
-      </Button>
-    </div>
-  );
+    const onClick = (provider: "google" | "github") => {
+        signIn(provider, {
+            callbackUrl: DEFAULT_LOGIN_REDIRECT,
+        });
+    };
+
+    return (
+        <div className=" flex items-center w-full gap-x-2">
+            <Button
+                size="lg"
+                className="w-full"
+                variant="outline"
+                onClick={() => onClick("google")}
+            >
+                <FcGoogle className="w-5 h-5" />
+            </Button>
+            <Button
+                size="lg"
+                className="w-full"
+                variant="outline"
+                onClick={() => onClick("github")}
+            >
+                <FaGithub className="w-5 h-5" />
+            </Button>
+        </div>
+    );
 };
